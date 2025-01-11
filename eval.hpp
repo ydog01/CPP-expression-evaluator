b7df345ea935a83d14d9e8f08584c7f82dee9157
ydog01 <1917927623@qq.com>
2025-01-11 17:15:17 +0800

    Add files via upload

---
+++ eval.hpp
@@ -0,0 +1,171 @@
+#ifndef eval_HPP
+#define eval_HPP
+#include"eval_C.hpp"
+namespace eval
+{
+    template<typename type>
+    size_t eval<type>::cpre(epre<type>& epre_,const std::string &str)
+    {
+        size_t flag = 0,Lpos;
+        typename sstree<func<type>>::iterator fptr;
+        typename sstree<var<type>>::iterator vptr;
+        std::vector<func<type> *> Lfuncs;
+        for (size_t pos = 0; pos < str.size(); pos++)
+        {
+            if (pos == flag)
+            {
+                if (str[pos] == '(')
+                {
+                    Lfuncs.push_back(nullptr);
+                    flag++;
+                }
+                else if(constbegin(str[pos]))
+                {
+                    Lpos=pos;
+                    while (++pos<str.size()&&constin(str[pos]));
+                    epre_.consts.push_back(stot(str.substr(Lpos, pos - Lpos)));
+                    epre_.index.push_back('c');
+                    pos--;
+                }
+                else
+                {
+                    Lpos = pos;
+                    if (fptr = funcs.first->find(str[pos]))
+                    {
+                        do
+                            funcs.first->setptr(fptr);
+                        while (++pos < str.size()&&str[pos]!='('&&(fptr = funcs.first->find(str[pos])));
+                        if((str[pos]=='(')&&(fptr->data))
+                        {
+                            Lfuncs.push_back(fptr->data);
+                            Lfuncs.push_back(nullptr);
+                            flag=++pos;
+                        }
+                        else
+                            pos=Lpos;
+                        funcs.first->rebegin();
+                    }
+                    if((pos==Lpos)&&(vptr=vars.first->find(str[pos])))
+                    {
+                        do
+                            vars.first->setptr(vptr);
+                        while(++pos<str.size()&&(vptr=vars.first->find(str[pos])));
+                        if(vars.first->data())
+                        {
+                            if(vars.first->data()->vtype==vartype::CONST)
+                            {
+                                epre_.consts.push_back(vars.first->data()->value);
+                                epre_.index.push_back('c');
+                            }
+                            else
+                            {
+                                epre_.vars.push_back(&(vars.first->data()->value));
+                                epre_.index.push_back('v');
+                            }
+                        }
+                        else
+                            pos=Lpos;
+                        vars.first->rebegin();
+                    }
+                    if((pos==Lpos)&&(fptr=oper1.first->find(str[pos])))
+                    {
+                        do
+                            oper1.first->setptr(fptr);
+                        while (++pos<str.size()&&(fptr=oper1.first->find(str[pos])));
+                        if(oper1.first->data())
+                        {
+                            Lfuncs.push_back(oper1.first->data());
+                            flag=pos;
+                        }
+                        else
+                            pos=Lpos;
+                        oper1.first->rebegin();
+                    }
+                    if(pos==Lpos)
+                        return pos;
+                    else
+                        pos--;
+                }
+            }
+            else
+            {
+                if(str[pos]==')')
+                {
+                    while(Lfuncs.size()&&Lfuncs.back())
+                    {
+                        epre_.funcs.push_back(Lfuncs.back());
+                        epre_.index.push_back('f');
+                        Lfuncs.pop_back();
+                    }
+                    if(Lfuncs.empty())
+                        return pos;
+                    Lfuncs.pop_back();
+                }
+                else if(fptr = oper2.first->find(str[pos]))
+                {
+                    do
+                        oper2.first->setptr(fptr);
+                    while(++pos<str.size()&&(fptr = oper2.first->find(str[pos])));
+                    if(oper2.first->data())
+                    {
+                        while(!Lfuncs.empty()&&Lfuncs.back()&&(Lfuncs.back()->flag>=oper2.first->data()->flag))
+                        {
+                            epre_.funcs.push_back(Lfuncs.back());
+                            epre_.index.push_back('f');
+                            Lfuncs.pop_back();
+                        }
+                        Lfuncs.push_back(oper2.first->data());
+                    }
+                    else
+                        return pos; 
+                    flag=pos--;
+                    oper2.first->rebegin();
+                }
+            }        
+        }
+        while(Lfuncs.size())
+        {
+            if(!Lfuncs.back())
+                return str.size();
+            epre_.funcs.push_back(Lfuncs.back());
+            epre_.index.push_back('f');
+            Lfuncs.pop_back();
+        }
+        return -1;
+    } 
+    template<typename type>
+    type eval<type>::result(const epre<type>& epre_)
+    {
+        size_t fpos=0,vpos=0,cpos=0,LSE;
+        type LRE;
+        std::vector<type> stack;
+        for(const char&ch:epre_.index)
+        {
+            switch(ch)
+            {
+            case 'f':
+                LSE = epre_.funcs[fpos]->size;
+                if (stack.size() < LSE)
+                    throw std::runtime_error("eval::result:stack size error");
+                LRE = epre_.funcs[fpos]->f(&(stack.back()) - LSE + 1);
+                while (LSE--)
+                    stack.pop_back();
+                stack.push_back(LRE);
+                fpos++;
+                break;
+            case 'v':
+                stack.push_back(*(epre_.vars[vpos++]));
+                break;
+            case 'c':
+                stack.push_back(epre_.consts[cpos++]);
+                break;
+            default:
+                throw std::runtime_error(std::string("eval::result:invalid index")+std::string(ch,1));
+            }
+        }
+        if(stack.size()!=1)
+            throw std::runtime_error("eval::result:stack size error");
+        return stack.back();
+    }
+}
+#endif
---
+++ eval_C.hpp
@@ -0,0 +1,96 @@
+#ifndef eval_CLASS
+#define eval_CLASS
+#include"sstree.hpp"
+#include<functional>
+#include<vector>
+namespace eval
+{
+    template <typename type>
+    struct func
+    {
+        size_t size;
+        size_t flag;
+        std::function<type(const type*)> f;
+    };
+    enum class vartype
+    {
+        CONST,
+        FREE
+    };
+    template <typename type>
+    struct var
+    {
+        vartype vtype;
+        type value;       
+    };
+    template<typename type>
+    struct epre
+    {
+        std::vector<func<type>*> funcs;
+        std::vector<type*> vars;
+        std::vector<type> consts;
+        std::string index;
+        void clear()
+        {
+            funcs.clear();
+            vars.clear();
+            consts.clear();
+            index.clear();
+        }
+    };
+    template<typename type>
+    struct eval
+    {
+        std::function<bool(const char&)> constbegin;
+        std::function<bool(const char&)> constin;
+        std::function<type(const std::string&)> stot;
+        std::pair<sstree<var<type>>*,bool> vars;
+        std::pair<sstree<func<type>>*,bool> funcs;
+        std::pair<sstree<func<type>>*,bool> oper1;
+        std::pair<sstree<func<type>>*,bool> oper2;
+
+        eval(std::function<bool(const char&)> constbegin_,
+        std::function<bool(const char&)> constin_,
+        std::function<type(const std::string&)> stot_):
+
+        constbegin(constbegin_),
+        constin(constin_),
+        stot(stot_),
+        vars{new sstree<var<type>>,true},
+        funcs{new sstree<func<type>>,true},
+        oper1{new sstree<func<type>>,true}, 
+        oper2{new sstree<func<type>>,true} {}
+
+        eval(std::function<bool(const char&)> constbegin_,
+            std::function<bool(const char&)> constin_,
+            std::function<type(const std::string&)> stot_,
+            sstree<var<type>>* vars_,
+            sstree<func<type>>* funcs_,
+            sstree<func<type>>* oper1_,
+            sstree<func<type>>* oper2_):
+        
+        constbegin(constbegin_),
+        constin(constin_),
+        stot(stot_),    
+        vars(vars?std::make_pair(vars_,false):std::make_pair(new sstree<var<type>>,true)),
+        funcs(funcs?std::make_pair(funcs_,false):std::make_pair(new sstree<func<type>>,true)),
+        oper1(oper1?std::make_pair(oper1_,false):std::make_pair(new sstree<func<type>>,true)), 
+        oper2(oper2_?std::make_pair(oper2_,false):std::make_pair(new sstree<func<type>>,true)) {}
+
+        ~eval()
+        {
+            if(vars.second)
+                delete vars.first;
+            if(funcs.second)
+                delete funcs.first;
+            if(oper1.second)
+                delete oper1.first;
+            if(oper2.second)
+                delete oper2.first;
+        }
+
+        size_t cpre(epre<type>& epre_,const std::string &str);
+        type result(const epre<type>& epre_);
+    };
+};
+#endif
